b6317b339bd6950176298b578c21501fbe48bbbc
Vladimir Surgai <v_surgay@mail.ru>
2016-06-12 23:11:45 +0300

    Code was formatted into PEP8

--- photos.py
+++ photos.py
@@ -19,8 +19,8 @@ generated_file_struct = defaultdict(list)
 files_number = 0
 for filename in os.listdir(args.source):
     full_path = args.source + "/" + filename
-    new_dir_name = time.strftime("%d.%m.%Y",time.localtime(os.path.getmtime(full_path)))
-    
+    new_dir_name = time.strftime("%d.%m.%Y", time.localtime(os.path.getmtime(full_path)))
+
     files_number = files_number + 1
     generated_file_struct[new_dir_name].append(filename)
 
@@ -31,12 +31,12 @@ for item in generated_file_struct:
     try:
         os.stat(new_dir_name)
     except:
-        os.mkdir(new_dir_name)  
+        os.mkdir(new_dir_name)
 
     for filename in generated_file_struct[item]:
         files_counter = files_counter + 1
         source = "{0}/{1}".format(args.source, filename)
         target = "{0}/".format(new_dir_name)
-        
-        print("[{0:.2f}%]".format(files_counter/ (files_number/100.0)))
+
+        print("[{0:.2f}%]".format(files_counter / (files_number / 100.0)))
         shutil.copy2(source, target)
